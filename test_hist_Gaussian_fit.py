e23b590e09aca6f7d337f86dc5a36c34865ead62
ZhiyuZhang <pmozhang@gmail.com>
2016-10-25 16:40:03 +0200

    adding p-value statistics test with fluxes before and after masking the largest value (outlier)

--- test_hist_Gaussian_fit.py
+++ test_hist_Gaussian_fit.py
@@ -16,6 +16,9 @@ f, (ax1, ax2, ax3, ax4) = plt.subplots(4, sharex=True, sharey=True)
 fluxes           = np.loadtxt("fluxes.dat")
 
 
+# - make statistics p-value test 
+scipy.stats.mstats.normaltest(fluxes)
+
 ## Following  http://stackoverflow.com/questions/7805552/fitting-a-histogram-with-python  
 
 bins             = np.linspace(0, 1, 40)
@@ -50,15 +53,20 @@ ax3.legend( loc=7, borderaxespad=0.)
 
 
 
-## use stats.normalpdf  
+## use stats.normalpdf, after masking the largest value at 1.43 
 
-fluxes           = fluxes[np.where(fluxes < 0.6)] 
+fluxes           = fluxes[np.where(fluxes < 0.8)] 
 bins             = np.linspace(0, 1, 40)
 (mu, sigma)      = norm.fit(fluxes)
 y                = norm.pdf( bins, mu, sigma)
 n, bins, patches = ax4.hist(fluxes, bins, normed=1, facecolor='green', alpha=0.75)
 l                = ax4.plot(bins, y, 'r--', linewidth=2, label='stats.norm.pdf-mask')
 ax4.legend(loc=7, borderaxespad=0.)
+
+
+# - make statistics p-value test again to the masked fluxes array
+scipy.stats.mstats.normaltest(fluxes)
+
 
 
 plt.savefig('test_hist_Gaussian_fit.pdf')
